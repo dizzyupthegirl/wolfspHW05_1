995b8ef47fdb75f2575c7fce724c491dad8d469e
Samantha Wolf <wolfsp@muohio.edu>
2012-11-29 16:53:20 -0500

    Working on addedge and weight

--- wolfspHW05_1/ListGraph.cpp
+++ wolfspHW05_1/ListGraph.cpp
@@ -12,6 +12,35 @@ ListGraph::~ListGraph(){
 
 }
 
+void ListGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight) {
+	EList:: const_iterator it;
 
+}
+
+EdgeWeight ListGraph::weight(NodeID u, NodeID v) const {
+	// We went over how to do this method in class.
+	EList:: const_iterator it;
+	for(it=edgeList[u].begin(); it!=edgeList[u].end(); it++) {
+		NWPair theEdge=*it;
+		if(theEdge.first==v) {
+			return theEdge.second;
+		}
+	}
+	return 0.0;
+}
+
+std::list<NWPair> ListGraph::getAdj(NodeID u) const {
+
+}
+
+unsigned ListGraph::degree(NodeID u) const {
+
+}
+
+unsigned ListGraph::size() const {
+
+}
+
+unsigned ListGraph::numEdges() const{
 
 }
