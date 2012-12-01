2a3bdb6c700eddf68941792e7d649847aa70c04e
Samantha Wolf <wolfsp@muohio.edu>
2012-11-30 20:51:44 -0500

    Fixed the problem, I had things equal to -1
    Apparrently they should start out at 0

--- wolfspHW05_1/MatrixGraph.cpp
+++ wolfspHW05_1/MatrixGraph.cpp
@@ -8,19 +8,21 @@ using namespace std;
 
 MatrixGraph::MatrixGraph(unsigned num_nodes) {
 	// Resizes matrix
+	num_edges=0;
 	M.resize(num_nodes);
-	for(int i=0; i<num_nodes; i++) {
+	for(int i=0; i<M.size(); i++) {
 		M[i].resize(num_nodes);
 	}
 	// Sets all weights equal to -1
-	for(int i=0; i<num_nodes; i++) {
+	for(int i=0; i<M.size(); i++) {
 		for(int k=0; k<num_nodes; k++) {
+
 				M[i][k]=0;
+
 			}
 		}
 	}
 	
-}
 
 MatrixGraph::~MatrixGraph() {
 }
@@ -28,6 +30,7 @@ MatrixGraph::~MatrixGraph() {
 void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight) {
 	M[v][u]=weight;
 	M[u][v]=weight;	
+	num_edges++;
 }
   
 EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const{
@@ -37,7 +40,7 @@ EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const{
 std::list<NWPair> MatrixGraph::getAdj(NodeID u) const{
 	std::list<NWPair>* adj= new std::list<NWPair>;
 	for(int i=0; i<M[u].size(); i++) {
-		if(M[u][i]!=-1) {
+		if(M[u][i]!=0) {
 			adj->push_back(std::make_pair(i, M[u][i]));
 		
 		}
@@ -50,7 +53,7 @@ unsigned MatrixGraph::degree(NodeID u) const{
 }
 
 unsigned MatrixGraph::size() const {
-	return M[0].size()*M[0].size();
+	return M.size();
 }
 
 unsigned MatrixGraph::numEdges() const {
